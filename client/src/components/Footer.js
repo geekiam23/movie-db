4dc162c135b3bac78022095e0050f0d0fb854c10
Will Larry <will23larry@gmail.com>
2018-07-31 12:20:48 -0400

    Changed components to be stateless

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Footer.css';
 
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <div id="movie-review-footer">
-        <footer className="movie-review-footer mdl-mega-footer">
-          <div className="mdl-mega-footer--top-section">
-            <div className="mdl-mega-footer--right-section">
-              <a className="mdl-typography--font-light" href="#top">
-                Back to Top
-                <i className="material-icons">expand_less</i>
-              </a>
-            </div>
-          </div>
-        </footer>
+const Footer = () => (
+  <div id="movie-review-footer">
+    <footer className="movie-review-footer mdl-mega-footer">
+      <div className="mdl-mega-footer--top-section">
+        <div className="mdl-mega-footer--right-section">
+          <a className="mdl-typography--font-light" href="#top">
+            Back to Top
+            <i className="material-icons">expand_less</i>
+          </a>
+        </div>
       </div>
-    )
-  }
-}
+    </footer>
+  </div>
+)
+
+export default Footer;
--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './MovieCard.css';
 
-export default class MovieCard extends React.Component{
-  render (){
-    return (
-      <div className="mdl-cell mdl-cell--4-col mdl-cell--7-col-tablet mdl-cell--7-col-phone mdl-card mdl-shadow--3dp movie-card__container">
-        <div className="mdl-card__supporting-text movie-review-card__container">
-          <Link to={`/movies/${this.props.id}`} >
-            <img className="movie-review-card__container-image" src={this.props.photo} alt={'movie-poster'}/>
-            <div className="mdl-typography--font-light mdl-typography--subhead">{this.props.title}</div>
-          </Link>
-            <div className="mdl-typography--font-light mdl-typography--subhead">{this.props.popularity}</div>
-            <div className="mdl-typography--font-light mdl-typography--subhead">{this.props.release_date}</div>
-            <div className="mdl-typography--font-light mdl-typography--subhead">{this.props.vote_average}</div>
-            <div className="mdl-typography--font-light mdl-typography--subhead">{this.props.overview}</div>
-            <div className="mdl-typography--font-light mdl-typography--subhead">{this.props.vote_count}</div>
-            <div className="mdl-typography--font-light mdl-typography--subhead">{this.props.adult}</div>
-        </div>
-      </div>
-    )
-  }
-}
+const MovieCard = (props) => (
+  <div className="mdl-cell mdl-cell--4-col mdl-cell--7-col-tablet mdl-cell--7-col-phone mdl-card mdl-shadow--3dp movie-card__container">
+    <div className="mdl-card__supporting-text movie-review-card__container">
+      <Link to={`/movies/${props.id}`} >
+        <img className="movie-review-card__container-image" src={props.photo} alt={'movie-poster'}/>
+        <div className="mdl-typography--font-light mdl-typography--subhead">{props.title}</div>
+      </Link>
+        <div className="mdl-typography--font-light mdl-typography--subhead">{props.popularity}</div>
+        <div className="mdl-typography--font-light mdl-typography--subhead">{props.release_date}</div>
+        <div className="mdl-typography--font-light mdl-typography--subhead">{props.vote_average}</div>
+        <div className="mdl-typography--font-light mdl-typography--subhead">{props.overview}</div>
+        <div className="mdl-typography--font-light mdl-typography--subhead">{props.vote_count}</div>
+        <div className="mdl-typography--font-light mdl-typography--subhead">{props.adult}</div>
+    </div>
+  </div>
+)
+
+export default MovieCard;
